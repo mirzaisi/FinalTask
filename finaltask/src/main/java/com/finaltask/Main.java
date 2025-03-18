f6e9b6a9b6f08612f9520d2333253b756e37948c
Mirza Isi <mirza.isi@std.ieu.edu.tr>
2025-03-18 22:19:33 +0300

    added main to run tests

--- finaltask/src/main/java/com/finaltask/Main.java
+++ finaltask/src/main/java/com/finaltask/Main.java
@@ -1,7 +1,14 @@
 package com.finaltask;
 
+import org.junit.runner.JUnitCore;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Running all UC tests...");
+        JUnitCore.runClasses(
+                UC1_EmptyCredentialsTest.class,
+                UC2_UsernameOnlyTest.class,
+                UC3_ValidCredentialsTest.class
+        );
     }
 }
